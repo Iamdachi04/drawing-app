c8482772371d81969b1b44a4bb07a4d96a8c51e1
Iamdachi04 <dachi4github@gmail.com>
2025-09-06 23:26:04 +0400

    function of drawing a circle.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let size = 10;
+
+function drawCircle(x,y){
+    ctx.beginPath();
+    ctx.arc(x,y,size,0,Math.PI*2, true);
+    ctx.fillStyle = 'red'; // Set a color for the circle
+    ctx.fill();
+}
+
+
+drawCircle(100,200);
