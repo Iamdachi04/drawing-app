29de34e5cab5d1882909a961cf450fb8f318ce88
Iamdachi04 <dachi4github@gmail.com>
2025-09-07 10:14:16 +0400

    added event listenerwhen the mouse just moves when it is pressed , and I used the already written functions like circledraw and linedraw , to draw what the person is trying to write , but I increased the sxe of the line that is being drawn y 2 , because as I understood the size of the circle means only the radius part , and so I increased the width of line by 2 so that it would match up with the circles width.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let size = 10;
+let size = 5;
 let color = 'black';
 let pressed=false;
 let x;
@@ -10,7 +10,7 @@ let y;
 function drawCircle(x,y){
     ctx.beginPath();
     ctx.arc(x,y,size,0,Math.PI*2, true);
-    ctx.fillStyle = 'red'; // Set a color for the circle
+    ctx.fillStyle = 'black'; // Set a color for the circle
     ctx.fill();
 }
 
@@ -20,7 +20,7 @@ ctx.beginPath();
 ctx.moveTo(x1,y1);
 ctx.lineTo(x2,y2);
 ctx.strokeStyle=color;
-ctx.lineWidth=size;
+ctx.lineWidth=size*2;
 ctx.stroke();
 }
 
@@ -35,3 +35,17 @@ canvas.addEventListener('mouseup',(e)=>{
     x=undefined;
     y= undefined;
 })   
+
+canvas.addEventListener('mousemove',(e)=>{
+    if(pressed){
+       const m = e.offsetX;
+       const n = e.offsetY;
+       
+      drawCircle(m,n);
+
+      drawLine(x,y,m,n);
+
+      x=m;
+      y=n;
+    }
+})
